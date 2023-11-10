ddb76933572f5aa1b58b40954aacd58f71e79c6b
flavio <flavio.kagohara40@gmail.com>
2023-11-10 15:38:27 -0300

    atualizando arquivos

---
+++ kafka/escrever_topico.py
@@ -0,0 +1,5 @@
+# Aqui como nós possuíamos um função padrão de escrita em tópicos, eu acabei não entrando tão a fundo, 
+# então não tenho certeza de como implementar, então vou utilizar o código apenas para escrever
+
+def escrever_em_topicos(TOPIC_NAME, created_at,lista_de_dados,updated_at)
+    #função para escrever no tópico
